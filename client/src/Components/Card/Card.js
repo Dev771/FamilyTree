93f0f7766d97aee8fe53d7ddc39e28c4f2265c75
devgarg <dev.garg@nagarro.com>
2024-01-03 16:11:22 +0530

    Day 4 (Not Much Basic Frontend etc) (Nothing much done this day)

---
+++ client/src/Components/Card/Card.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div className=''>
+        <img />
+        <div className='d-flex flex-column'>
+            <h1>Dev Garg</h1>
+            <div className='d-flex gap-1'>
+                
+            </div>
+        </div> 
+    </div>
+  )
+}
+
+export default Card;
---
+++ client/src/Components/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Card from '../Card/Card'
+
+import './Home.css';
+
+const Home = () => {
+  return (
+    <main className='d-flex justify-content-center align-items-center'>
+        <Card />
+    </main>
+  )
+}
+
+export default Home
